94d66b9cdd4bff3fa87ea49a6dc2521aba6df343
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2021-09-01 20:23:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,53 @@ const inputEl=document.getElementById("input-el")
 //const cannot be reassigned
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn=document.getElementById("delete-btn")
+const tabBtn=document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage)
+{
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true},function(tabs)
+    {
+        myLeads.push(tabs[0].url)
+       localStorage.setItem("myLeads",JSON.stringify(myLeads))
+       render(myLeads)
+    })
+      
+
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    console.log("double clicked")
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEl.value)
     inputEl.value=""
-    renderLeads()
+
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
     
 })
 
-function renderLeads(){
+function render(leads){
     let listItems=""
 //render the leads in the unordered list
-for(let i=0;i<myLeads.length;i++)
+for(let i=0;i<leads.length;i++)
 {
         listItems+= `
         <li>
-        <a target='_blank' href='${myLeads[i]}'>  
-            ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>  
+            ${leads[i]}
             </a>
         </li>
         `
@@ -29,4 +59,3 @@ for(let i=0;i<myLeads.length;i++)
 ulEl.innerHTML =listItems
 
 }
-"Hey" 
